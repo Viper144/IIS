508e4f18ee2cb85c7882e82d9ae8095df8dd0592
Viper144 <ananev.1234@gmail.com>
2023-04-16 14:23:49 +0300

    снизил число

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,31 @@
 ﻿
 
         Console.WriteLine("Введите сумму ежемесячного пополнения");
-        string text = Console.ReadLine();
+        string? text = Console.ReadLine();
         int money = Convert.ToInt32(text);
 
         if (money < 1000)
         {
             Console.WriteLine("Извините, вы слишком нищий для нашего банка");
-        }
-        else
-        {
-            Console.WriteLine("Сумма дивидендов за год:");
+    return;        
+}
+Console.WriteLine("Сумма дивидендов за год:");
 
-            int a = money * 12;
-            double c;
-            if (money < 5000)
-            {
-                c = 0.1;
-            }
+int a = money * 10;
 
-            else
-            {
-                c = 0.3;
-            }
-            double b = a * c;
-            double d = Convert.ToInt32(b);
-            double x = a + d;
-            x = Convert.ToInt32(x);
-            Console.WriteLine(x);
-        }
-        Console.ReadKey();
+double c;
+if (money < 5000)
+{
+    c = 0.1;
+}
+
+else
+{
+    c = 0.3;
+}
+double b = a * c;
+double d = Convert.ToInt32(b);
+double x = a + d;
+x = Convert.ToInt32(x);
+Console.WriteLine(x);
+Console.ReadKey();
