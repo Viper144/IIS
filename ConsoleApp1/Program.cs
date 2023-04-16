5b0ba42dc641664419e57203828faa2fb3b9204b
Viper144 <ananev.1234@gmail.com>
2023-04-16 14:21:40 +0300

    исправил gitignore

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,32 @@
-﻿Console.WriteLine("Введите сумму ежемесячного пополнения");
-string text = Console.ReadLine();
-int money = Convert.ToInt32(text);
+﻿
 
-if (money < 1000)
-{
-    Console.WriteLine("Извините, вы слишком нищий для нашего банка");
-}
-else
-{
-    Console.WriteLine("Сумма дивидендов за год:");
+        Console.WriteLine("Введите сумму ежемесячного пополнения");
+        string text = Console.ReadLine();
+        int money = Convert.ToInt32(text);
 
-    int a = (money * 12);
-    double c;
-    if (money < 5000) {
-      c = 0.1; 
-    }
+        if (money < 1000)
+        {
+            Console.WriteLine("Извините, вы слишком нищий для нашего банка");
+        }
+        else
+        {
+            Console.WriteLine("Сумма дивидендов за год:");
 
-    else {
-      c = 0.3; 
-    }
-    double b = a * c;
-    double d = Convert.ToInt32(b);
-    double x = a + d;
-    x = Convert.ToInt32(x);
-    Console.WriteLine(x);
-}
+            int a = money * 12;
+            double c;
+            if (money < 5000)
+            {
+                c = 0.1;
+            }
+
+            else
+            {
+                c = 0.3;
+            }
+            double b = a * c;
+            double d = Convert.ToInt32(b);
+            double x = a + d;
+            x = Convert.ToInt32(x);
+            Console.WriteLine(x);
+        }
+        Console.ReadKey();
